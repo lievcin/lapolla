6873ad753e80314cb8f5bcf80c4222e4a771907d
Liev <liev.garcia@gmail.com>
2012-07-03 20:02:25 +0100

    inits

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,13 +1,2 @@
-module GamesHelper
-  
-  def use_zone(time_zone)
-    new_zone = find_zone!(time_zone)
-    begin
-      old_zone, ::Time.zone = ::Time.zone, new_zone
-      yield
-    ensure
-      ::Time.zone = old_zone
-    end
-  end
-  
+module GamesHelper 
 end
